8b7a4d9af98b39722c30434b01265f7d1ebab0e1
Eta <pleasefckoff00@gmail.com>
2024-11-11 14:21:20 +0000

    Added Better Embeds

--- src/index.js
+++ src/index.js
@@ -21,31 +21,34 @@ client.on("interactionCreate", (interaction) => {
     if (interaction.commandName === "rules") {
 
         const rules_embed = new EmbedBuilder()
-            .setColor(0x0099FF)
-            .setTitle("Among us")
-            .setAuthor({ name: '@project_eta'})
-            .addFields(
-                {
-                    name: "Roles",
-                    value: "- Imposter (1)\n - Innocent (3)"
-                },
-                {
-                    name: "Basic Game Rules",
-                    value: "- Innocents Win if the Imposter Dies.\n - Innocents Win if they complete all option tasks + Guess the Correct Ghost.\n - Imposters Win when all innocents dies OR they are unable to complete the objectives.\n - Innocents can not touch the cursed object.\n - Imposters can hide equipment, however they cannot glitch or hide it in places unable to be reached.\n - Nobody can talk while dead unless they are the imposter.\n - The imposter can not take intentionally bad photos."
-                },
-                {
-                    name: "Player - Custom Game Rules",
-                    value: "Starting Sanity: 100\nSanity Pill Restroation: 40\nSanity Drain: 100\nSprinting: On\nPlayer Speed: 100\nFlashlights: On\nLose Items and Cosumables: off"
-                },
-                {
-                    name: "Ghost - Custom Game Rules",
-                    value: "Ghost Speed: 100\nRoaming Frequency: Medium\nChanging Favourite Room: Medium\nActivity Level: Medium\nEvent Frequency: Low\nFriendly Ghost: off\nGrace Period(s): 0\nHunt Duration: Medium\nKills Extend Hunts: Low\nEvidence Given: 2"
-                },
-                {
-                    name: "Contract - Custom Game Rules",
-                    value: "Setup Time: 60\nWeather: Random\nDoors Start Open: High\nNumber of Hiding Places: Low\nSanity Montior: off\nActivity Monitor: off\nFuse box at start of contract: off\nFuse Box visible on map: off\nCursed Possessions Quanitity: 3(random)"
-                },
-            )
+        .setAuthor({
+          name: "Phasmophobia Among Us",
+          url: "https://github.com/ProjektEta/Phaso-Amongus",
+        })
+        .setTitle("Among Us Rules.")
+        .addFields(
+          {
+            name: "Roles",
+            value: "There is 1 Imposter & 3 Innocents.\n\nThe Imposter needs to sabotage the innocents and help the ghost in killing them/hiding the ghosts type. They can do this by any means.\n\nThe Innocents need to guess the correct ghost type, survive and do all the optional tasks.",
+            inline: false
+          },
+          {
+            name: "Basic Game Rules.",
+            value: "The Imposter is the only person allowed to use/pick up the cursed object.\nThe Imposter can hide equipment where it can be found by innocents.\nThe Imposter CAN'T take photos badly on purpose.\nThe Imposter CAN'T refuse to vote on photo deletion.\nThe Imposter CAN'T waste equipment on purpose (like using all the incense, salt)\nThe Imposter CAN'T make the truck leave unless they are the last one left.\nThe Imposter Must communicate that they are the imposter when they die.\nThe Innocents can not talk while dead.\nAt the end of the game, dead innocents and the imposters ghost guesses will not be counted.",
+            inline: false
+          },
+          {
+            name: "End Game Conditions.",
+            value: "Imposter Wins by the following\n- All The Innocents Die.\n- The Innocents gets the Ghost type wrong\n- The Innocents can no longer be able to get one of the optional tasks\n\nInnocents Win\n- The Imposter Dies\n- Do all tasks and get the correct Ghost type.\n\nAt the end of the game, all ALIVE innocents must have the correct Ghost type, if one of them doesnt have the correct ghost type it counts as the innocents getting it wrong.\n\nThe Game Officially ends when the truck is leaving.",
+            inline: false
+          },
+        )
+        .setColor("#00b0f4")
+        .setFooter({
+          text: "Made by @project Eta",
+        })
+        .setTimestamp();
+      
 
         interaction.reply({embeds: [rules_embed]})
     }
@@ -61,36 +64,62 @@ client.on("interactionCreate", (interaction) => {
         var randomChoice = Math.floor(Math.random() * 3);
 
         const innocentEmbed = new EmbedBuilder()
-            .setColor(0x0099FF)
-            .setTitle("Among us")
-            .setAuthor({ name: '@project_eta'})
-            .setDescription("You are Innocent")
-            .addFields(
-                {
-                    name: 'Role Rules',
-                    value: '- You need to complete all optional tasks + get the correct ghost.\n - Find out who the imposter is. \n - Game ends when the imposter dies, the innocents did all the tasks or When all innocents die.'
-                },
-                {
-                    name: 'Basic Game Rules',
-                    value: '- You cant touch/use the cursed object. \n - You cant communicate while dead.'
-                },
-            )
+        .setAuthor({
+          name: "Phasmophobia Among Us",
+          url: "https://github.com/ProjektEta/Phaso-Amongus",
+        })
+        .setTitle("You Are Innocent.")
+        .addFields(
+          {
+            name: "Role Rules",
+            value: "Guess the Correct Ghost.\nComplete All Optional Tasks.\nYou can't touch or use the cursed object.\nDo not die.",
+            inline: false
+          },
+          {
+            name: "Basic Game Rules",
+            value: "Innocents can't use the cursed object, but imposters can.\nYou can't communicate while dead.",
+            inline: false
+          },
+          {
+            name: "End Conditions.",
+            value: "All Innocents Die.\nInnocents Do all the Tasks Required (And guess the correct ghost.)\nImposter Dies.\n\nIf the innocents leave, and one of the tasks isn't complete or they got the wrong ghost, Imposters win.\n\nEvery Innocent must have the correct Ghost. if 1 Innocent has it wrong (unless they are dead) then Imposters win.",
+            inline: false
+          },
+        )
+        .setColor("#45fc03")
+        .setFooter({
+          text: "Made by @project Eta",
+        })
+        .setTimestamp();
 
         const imposterEmbeds = new EmbedBuilder()
-            .setColor(0x0099FF)
-            .setTitle("Among us")
-            .setAuthor({ name: '@project_eta'})
-            .setDescription("You are the Imposter")
-            .addFields(
-                {
-                    name: 'Role Rules',
-                    value: '- You need to stop the innocents from getting all the tasks and finding the correct ghost.\n - Stop them by shouting fake evidence. Hiding equipment. Any means. \n - Game ends when the imposter dies, the innocents did all the tasks or When all innocents die.'
-                },
-                {
-                    name: 'Basic Game Rules',
-                    value: '- You can use any cursed objects at your disposal. \n - You must say if your the imposter when you have died.'
-                },
-            )
+        .setAuthor({
+          name: "Phasmophobia Among Us",
+          url: "https://github.com/ProjektEta/Phaso-Amongus",
+        })
+        .setTitle("You Are The Imposter")
+        .addFields(
+          {
+            name: "Role Rules",
+            value: "Stop the Innocents from guessing the correct ghost and complete the tasks.\nYou are allowed to hide equipment.\nYou can use the Cursed Object.\nYou can say anything that is false/not true, to trick the innocents.",
+            inline: false
+          },
+          {
+            name: "Basic Game Rules",
+            value: "Imposters Can use the cursed object.\nYou are not allowed to take photos that are purposefully bad. However you are allowed to hide the Photo cameras.\nYou are not allowed to hide items outside the map where they can not be found.\nWhen you die you must communicate that you was the imposter.",
+            inline: false
+          },
+          {
+            name: "End Conditions.",
+            value: "All Innocents Die.\nInnocents Do all the Tasks Required (And guess the correct ghost.)\nImposter Dies.\n\nIf the innocents leave, and one of the tasks isn't complete or they got the wrong ghost, Imposters win.\n\nEvery Innocent must have the correct Ghost. if 1 Innocent has it wrong (unless they are dead) then Imposters win.",
+            inline: false
+          },
+        )
+        .setColor("#fc031c")
+        .setFooter({
+          text: "Made by @project Eta",
+        })
+        .setTimestamp();
         if (randomChoice === 0) {
             // Player 1
             player1.user.send({embeds: [imposterEmbeds]})
