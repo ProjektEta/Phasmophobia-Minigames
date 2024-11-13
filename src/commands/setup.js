30c93e6c14db7e5d23110678d83a5e4ad31b5544
Eta <pleasefckoff00@gmail.com>
2024-11-13 11:30:18 +0000

    Updated with the new Embeds.js file.

--- src/commands/setup.js
+++ src/commands/setup.js
@@ -1,33 +1,7 @@
-const { EmbedBuilder } = require("discord.js");
+const { AmongUS_Setup_Embed } = require("../tools/embeds.js")
 
 const execute = function(interaction) {
-
-    const embed = new EmbedBuilder()
-    .setAuthor({
-      name: "Phasmophobia Among Us",
-      url: "https://github.com/ProjektEta/Phaso-Amongus",
-    })
-    .setTitle("Phasmophobia Gamemode Setup")
-    .addFields(
-      {
-        name: "Player Rules",
-        value: "Starting Sanity: 100\nSanity Pill Restoration: 40\nSanity Drain Speed: 100\nSprinting: On\nPlayer Speed: 100\nFlashlights: On\nLose Items and Consumables: Off",
-        inline: false
-      },
-      {
-        name: "Ghost Rules",
-        value: "Ghost Speed: 100\nRoaming Frequency: Medium\nChanging Favourite Room: Medium\nActivity Level: Medium\nEvent Frequency: Low\nFriendly Ghost: Off\nGrace Period(s): 0\nHunt Duration: Medium\nKills Extend Hunts: Low\nEvidence Given: 2",
-        inline: false
-      },
-      {
-        name: "Contract Rules",
-        value: "Setup Time: 60\nWeather: Random\nDoors Starting Open: High\nNumber of hiding Places: Low\nSanity Monitor: Off\nActivity Monitor: Off\nFuse Box at start of Contract: Off\nFuse box visible on map: Off\nCursed Possessions Quantity: 3 (All random)",
-        inline: false
-      },
-    )
-    .setColor("#00b0f4");
-
-    interaction.reply({embeds: [embed]})
+  interaction.reply({embeds: [AmongUS_Setup_Embed]})
 }
 
 module.exports = { execute }
